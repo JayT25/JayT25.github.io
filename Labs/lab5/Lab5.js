497076c08ab6bdb5616d4d5dab29237b4436f31c
jude <54775214+JayT25@users.noreply.github.com>
2021-05-25 17:27:14 -0400

    Update Lab5.js

--- Labs/lab5/Lab5.js
+++ Labs/lab5/Lab5.js
@@ -1,8 +1,8 @@
 var barking = new Audio('Labs/lab5/barking.wav');
 var chime = new Audio('Labs/lab5/chime.wav');
 var piano = new Audio('Labs/lab5/piano.wav');
-var camera = new Audio('Labs/lab5/camera_shutter.wav');
-var lion = new Audio('Labs/lab5/lion_roar.wav');
+var camera = new Audio('Labs/lab5/camera.wav');
+var lion = new Audio('Labs/lab5/lion.wav');
 
 
 function playAudio(pA)
